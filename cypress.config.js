5967b9e1f7568d3c8f79febc488712bf96d3a535
Gabriel Santana <santanna.gab@gmail.com>
2024-06-03 18:05:24 -0300

    configurando git actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = defineConfig({
+    projectId: "52h3e3",
     e2e: {
         specPattern: "cypress/e2e/features",
         setupNodeEvents(on, config) {
