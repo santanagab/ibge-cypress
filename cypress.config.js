4a766536273487bf5affbdbef15393160dd02298
Gabriel Santana <santanna.gab@gmail.com>
2024-06-03 18:26:48 -0300

    algus ajustes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+
 module.exports = defineConfig({
-    projectId: "52h3e3",
+
+    plugins: {
+        "cypress-cucumber-preprocessor": {
+            nonGlobalStepDefinitions: true
+        }
+    },
+
     e2e: {
         specPattern: "cypress/e2e/features",
         setupNodeEvents(on, config) {
             on("file:preprocessor", cucumber());
-        },
-    },
+        }
+    }
 });
